111f87efecc35f863ff0cb833a5c4baf3a59bd68
shahar <shaharbar2@gmail.com>
2023-11-30 22:11:08 +0200

    Tool work better and look better

--- Assets/_SBTools/SBGitCommander.cs
+++ Assets/_SBTools/SBGitCommander.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
@@ -7,34 +8,96 @@ using System.Linq;
 
 namespace Shahar.Bar.Utils
 {
-    public class SbGitCommander : EditorWindow
+    public class SBGitCommander : EditorWindow
     {
-        private List<string> _gitCommands = new();
-        private int _selectedCommandIndex;
+        private Dictionary<string, string> _gitCommands = new();
+        private string _selectedCommandKey;
+        private int _selectedCommandIndex = -1;
         private string _commandOutput = string.Empty;
         private Vector2 _scrollPosition;
-        private string _filePath = "Assets/GitCommands.txt";
+
+        private readonly string _fullPath = Path.Combine(Application.dataPath, "..", "SBTools", "GitCommands.txt");
 
         private string _newCommand;
+        private string _newCommandNickname;
 
         [MenuItem("SBTools/Git Commander")]
         private static void Init()
         {
-            var window = (SbGitCommander)GetWindow(typeof(SbGitCommander));
+            var window = (SBGitCommander)GetWindow(typeof(SBGitCommander));
             window.titleContent = new GUIContent("Git Commander");
+            window.EnsureDirectoryAndFileExist();
             window.LoadCommands();
+            window.RefreshInfo();
             window.Show();
+        }
+
+        private void EnsureDirectoryAndFileExist()
+        {
+            var directoryInfo = new DirectoryInfo(_fullPath).Parent;
+
+            if (directoryInfo == null) return;
+            Directory.CreateDirectory(directoryInfo.FullName);
+
+            if (File.Exists(_fullPath)) return;
+
+            File.Create(_fullPath).Dispose();
+            AddDefaultCommands();
+        }
+
+
+        private void AddDefaultCommands()
+        {
+            AddCommand("Pull", "git pull");
+            AddCommand("Push", "git push");
+            AddCommand("Status", "git status");
+            AddCommand("Add All", "git add .");
+            AddCommand("Commit", "git commit -m \"\"");
         }
 
         private void OnGUI()
         {
-            AddEditComands();
+            try
+            {
+                DisplayDropDownCommands();
 
-            InvokeOrDeleteCommands();
+                CommandInfo();
 
-            DisplayOutput();
+                Buttons();
+
+                DisplayOutput();
+            }
+            catch (Exception ex)
+            {
+                EditorGUILayout.HelpBox("An error occurred: " + ex.Message, MessageType.Error);
+            }
 
             TrySaveCommands();
+        }
+
+        private void InvokeButton()
+        {
+            GUIStyle invokeStyle = new(GUI.skin.button)
+            {
+                normal =
+                {
+                    textColor = Color.green
+                },
+                fontStyle = FontStyle.Bold,
+            };
+
+            if (GUILayout.Button("Invoke", invokeStyle, GUILayout.Width(100)))
+            {
+                ExecuteCommand(_gitCommands[_selectedCommandKey]);
+            }
+        }
+
+        private void Buttons()
+        {
+            GUILayout.BeginHorizontal();
+            AddEditCommands();
+            DeleteButton();
+            GUILayout.EndHorizontal();
         }
 
         private void TrySaveCommands()
@@ -47,80 +110,119 @@ namespace Shahar.Bar.Utils
 
         private void DisplayOutput()
         {
-            GUILayout.Label("Command Output", EditorStyles.boldLabel);
-            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, GUILayout.Height(100));
-            GUILayout.TextArea(_commandOutput, GUILayout.ExpandHeight(true));
+            var outputStyle = new GUIStyle(GUI.skin.window)
+            {
+                richText = true,
+                normal = { background = Texture2D.blackTexture, textColor = Color.green },
+                hover = { background = Texture2D.blackTexture, textColor = Color.green },
+                focused = { background = Texture2D.blackTexture, textColor = Color.green },
+                active = { background = Texture2D.blackTexture, textColor = Color.green },
+                stretchHeight = true,
+                wordWrap = true,
+                alignment = TextAnchor.UpperLeft
+            };
+            
+            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, GUILayout.Height(300));
+            GUILayout.TextArea(_commandOutput, outputStyle, GUILayout.ExpandHeight(true));
             GUILayout.EndScrollView();
         }
 
-        private void InvokeOrDeleteCommands()
+        private void DeleteButton()
         {
-            GUILayout.Space(10);
-
-            GUILayout.Label("Git Commands", EditorStyles.boldLabel);
-
-            if (_gitCommands.Count > 0)
+            GUIStyle deleteStyle = new(GUI.skin.button)
             {
-                _selectedCommandIndex = EditorGUILayout.Popup("Commands", _selectedCommandIndex, _gitCommands.ToArray());
+                normal =
+                {
+                    textColor = Color.red
+                }
+            };
+
+            if (_gitCommands.Count > 0 && GUILayout.Button("Delete", deleteStyle, GUILayout.Width(100)))
+            {
+                _gitCommands.Remove(_selectedCommandKey);
+                _selectedCommandKey = _gitCommands.Keys.FirstOrDefault();
             }
+        }
+
+        private void DisplayDropDownCommands()
+        {
+            if (_gitCommands.Count <= 0) return;
 
             GUILayout.BeginHorizontal();
 
-            if (GUILayout.Button("Invoke"))
-            {
-                ExecuteCommand(_gitCommands[_selectedCommandIndex]);
-            }
-
-            if (_gitCommands.Count > 0 && GUILayout.Button("Remove"))
-            {
-                _gitCommands.RemoveAt(_selectedCommandIndex);
-                _selectedCommandIndex = 0;
-            }
-
+            var prevIndex = _selectedCommandIndex;
+            _selectedCommandIndex = EditorGUILayout.Popup(_selectedCommandIndex, _gitCommands.Keys.ToArray());
+            _selectedCommandKey = _gitCommands.Keys.ElementAtOrDefault(_selectedCommandIndex);
+            InvokeButton();
             GUILayout.EndHorizontal();
+
+            if (prevIndex == _selectedCommandIndex) return;
+
+            RefreshInfo();
         }
 
-        private void AddEditComands()
+        private void RefreshInfo()
         {
-            GUILayout.Label("Add New Command", EditorStyles.boldLabel);
+            _newCommand = _gitCommands[_selectedCommandKey];
+            _newCommandNickname = _selectedCommandKey;
+        }
+
+        private void AddEditCommands()
+        {
+            if (GUILayout.Button("New Command"))
+            {
+                AddCommand(_newCommandNickname, _newCommand);
+            }
+
+            if (_gitCommands.Count > 0 && GUILayout.Button("Override Chosen"))
+            {
+                _gitCommands[_selectedCommandKey] = _newCommand;
+            }
+        }
+
+        private void CommandInfo()
+        {
+            var infoStyle = new GUIStyle(EditorStyles.helpBox)
+            {
+                padding = new RectOffset(10, 10, 10, 10),
+                margin = new RectOffset(5, 5, 5, 5),
+            };
+
+            EditorGUILayout.BeginVertical(infoStyle);
+
+            _newCommandNickname = EditorGUILayout.TextField("Nickname", _newCommandNickname);
             _newCommand = EditorGUILayout.TextField("Command", _newCommand);
 
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Add Command"))
-            {
-                AddCommand(_newCommand);
-            }
-
-            if (_gitCommands.Count > 0 && GUILayout.Button("Edit"))
-            {
-                _gitCommands[_selectedCommandIndex] = _newCommand;
-            }
-
-            GUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
         }
 
-        private void AddCommand(string command)
+
+        private void AddCommand(string nickname, string command)
         {
-            if (!string.IsNullOrWhiteSpace(command) && !_gitCommands.Contains(command))
+            if (!string.IsNullOrWhiteSpace(command) && !string.IsNullOrWhiteSpace(nickname) && !_gitCommands.ContainsKey(nickname))
             {
-                _gitCommands.Add(command);
+                _gitCommands.Add(nickname, command);
                 SaveCommands();
             }
         }
 
         private void SaveCommands()
         {
-            File.WriteAllLines(_filePath, _gitCommands);
+            var folderPath = Path.Combine(Application.dataPath, "SBTools");
+            Directory.CreateDirectory(folderPath);
+
+            var lines = _gitCommands.Select(kvp => $"{kvp.Key}|{kvp.Value}");
+            File.WriteAllLines(_fullPath, lines);
         }
 
         private void LoadCommands()
         {
-            _filePath = Path.Combine(Application.dataPath, "..", "SBTools");
+            if (!File.Exists(_fullPath)) return;
 
-            if (File.Exists(_filePath))
-            {
-                _gitCommands = File.ReadAllLines(_filePath).ToList();
-            }
+            var lines = File.ReadAllLines(_fullPath);
+            _gitCommands = lines.ToDictionary(
+                line => line.Split('|')[0],
+                line => line.Split('|').Length > 1 ? line.Split('|')[1] : "");
         }
 
         private void ExecuteCommand(string command)
@@ -137,6 +239,7 @@ namespace Shahar.Bar.Utils
                     CreateNoWindow = true
                 }
             };
+            
             process.Start();
             var output = process.StandardOutput.ReadToEnd();
             var error = process.StandardError.ReadToEnd();
